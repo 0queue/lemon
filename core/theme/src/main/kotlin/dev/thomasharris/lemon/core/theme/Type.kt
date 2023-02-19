5f64fd3e14986a7019d971156e8f9f70cb694448
Thomas Harris <ta.harris.42@gmail.com>
2023-02-19 11:52:46 +0100

    Experiment with the archivo font

--- core/theme/src/main/kotlin/dev/thomasharris/lemon/core/theme/Type.kt
+++ core/theme/src/main/kotlin/dev/thomasharris/lemon/core/theme/Type.kt
@@ -1,34 +1,87 @@
 package dev.thomasharris.lemon.core.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontVariation
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 22.sp,
-        letterSpacing = 0.5.sp,
+// using:
+// - titleMedium
+// - titleLarge
+// - bodySmall
+// - labelLarge
+
+
+// for archivo at least
+object FontWidth {
+    val ExtraCondensed = FontVariation.width(62.5f)
+    val Condensed = FontVariation.width(75f)
+    val SemiCondensed = FontVariation.width(87.5f)
+    val Normal = FontVariation.width(100f)
+    val SemiExpanded = FontVariation.width(112.5f)
+    val Expanded = FontVariation.width(125f)
+}
+
+
+@OptIn(ExperimentalTextApi::class)
+val archivo = FontFamily(
+    Font(
+        resId = R.font.archivo_variable,
+        weight = FontWeight.Normal,
+        style = FontStyle.Normal,
+        variationSettings = FontVariation.Settings(
+            weight = FontWeight.Normal,
+            style = FontStyle.Normal,
+            FontWidth.Normal,
+        ),
     ),
-        /* Other default text styles to override
+    Font(
+        resId = R.font.archivo_variable,
+        weight = FontWeight.Medium,
+        style = FontStyle.Normal,
+        variationSettings = FontVariation.Settings(
+            weight = FontWeight.Medium,
+            style = FontStyle.Normal,
+            FontWidth.Normal,
+        ),
+    ),
+    Font(
+        resId = R.font.archivo_italic_variable,
+        weight = FontWeight.Normal,
+        style = FontStyle.Italic,
+        variationSettings = FontVariation.Settings(
+            weight = FontWeight.Normal,
+            style = FontStyle.Italic,
+            FontWidth.Normal,
+        ),
+    ),
+)
+
+val Typography = Typography(
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = archivo,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.2.sp,
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    titleMedium = TextStyle(
+        fontFamily = archivo,
+        fontWeight = FontWeight.Normal,
+        fontSize = 18.sp, // from 16
+        lineHeight = 28.sp,
+        letterSpacing = 0.2.sp,
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = archivo,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 20.sp,
+        letterSpacing = 0.5.sp,
+    ),
 )
