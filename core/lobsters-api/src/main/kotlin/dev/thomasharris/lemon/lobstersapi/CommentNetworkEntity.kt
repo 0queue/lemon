14f2b55d15003dbf17d5f757a82b2a10e128d72f
Thomas Harris <ta.harris.42@gmail.com>
2025-02-16 13:01:48 +0100

    fix: updated_at is now last_edited_at

--- core/lobsters-api/src/main/kotlin/dev/thomasharris/lemon/lobstersapi/CommentNetworkEntity.kt
+++ core/lobsters-api/src/main/kotlin/dev/thomasharris/lemon/lobstersapi/CommentNetworkEntity.kt
@@ -9,7 +9,7 @@ data class CommentNetworkEntity(
     @SerialName("short_id") val shortId: ShortId,
     @SerialName("short_id_url") val shortIdUrl: String,
     @SerialName("created_at") val createdAt: Instant,
-    @SerialName("updated_at") val updatedAt: Instant,
+    @SerialName("last_edited_at") val updatedAt: Instant,
     @SerialName("is_deleted") val isDeleted: Boolean,
     @SerialName("is_moderated") val isModerated: Boolean,
     val score: Int,
